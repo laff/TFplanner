bb35d9c659304f807f05ade500a1e4ef059967bd
Christian Simensen <chr-sime@online.no>
2014-04-29 12:33:42 +0200

    Manual merge

--- drawer/grid.js
+++ drawer/grid.js
@@ -413,7 +413,6 @@ Grid.prototype.save = function(callback) {
 
     trEle = doc.createElement('tr');
 
-<<<<<<< HEAD
     trEle = document.createElement('tr');
 
     // create and add header 'ELNUMMER'  (number)
@@ -424,11 +423,7 @@ Grid.prototype.save = function(callback) {
     // create and add header 'PRODUKTEBSKRIVELSE' (desc)
     thEle = document.createElement('th');
     thEle.innerHTML = 'PRODUKTBESKRIVELSE';
-=======
-    // Create and add header 'productnumber'
-    thEle = doc.createElement('th');
-    thEle.innerHTML = 'Produktnummer';
->>>>>>> 8c799e2b48f716a45b8deaf4bceb529831839439
+
     trEle.appendChild(thEle);
 
     // Create and add header 'name'
@@ -436,16 +431,11 @@ Grid.prototype.save = function(callback) {
     thEle.innerHTML = 'Beskrivelse';
     trEle.appendChild(thEle);
 
-<<<<<<< HEAD
     // create and add header 'ANTALL' (amount)
     thEle = document.createElement('th');
     thEle.className = 'amount';
     thEle.innerHTML = 'ANTALL';
-=======
-    // Create and add header 'amount'
-    thEle = doc.createElement('th');
-    thEle.innerHTML = 'Antall';
->>>>>>> 8c799e2b48f716a45b8deaf4bceb529831839439
+
     trEle.appendChild(thEle);
 
     // Add header row to table
@@ -468,12 +458,8 @@ Grid.prototype.save = function(callback) {
         trEle.appendChild(tdEle);
 
         // Add column 'amount' to row
-<<<<<<< HEAD
         tdEle = document.createElement('td');
         tdEle.className = 'amount';
-=======
-        tdEle = doc.createElement('td');
->>>>>>> 8c799e2b48f716a45b8deaf4bceb529831839439
         tdEle.innerHTML = mats[i][2];
         trEle.appendChild(tdEle);
 
