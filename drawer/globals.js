8a7efe8ecdb7ebcfb201de45feed0206b30dbfbc
Christian Simensen <chr-sime@online.no>
2014-05-09 15:07:54 +0200

    Comments changed to satisfy JSDoc

--- drawer/globals.js
+++ drawer/globals.js
@@ -1,6 +1,16 @@
 /**
- * @global
- * Namespace with the global TFplanner
+ * @namespace TFplanner
+ * Namespace for keeping variable-names hidden for other scripts
+ * @property {Grid} - Grid of the software
+ * @property {ScrollBox} - Zoom- and panfunctionality
+ * @property {Measurement} - Measures wall-lengths and angles
+ * @property {DrawRoom} - Setting up drawing of a room
+ * @property {Tabs} - Tab-functionality
+ * @property {FootMenu} - FootMenu of the GUI, hold buttons
+ * @property {Options} - Shows static pages for the user
+ * @property {Obstacles} - Used when adding obstacles
+ * @property {FinishedRoom} - A room finished by the user, initially 'null'
+ * @property {latency} - Latency used for move-actions
 **/
 var TFplanner =  TFplanner || {};
 
